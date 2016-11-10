126ae45674b7bdd50967c1f2a0dc74eb2798325e
Max Heiber <max.heiber@gmail.com>
2016-11-09 19:11:11 -0500

    tests: use tape instead of assert lib
    - because Node maintainers say not to use assert
    - because assert is buggy and frozen
    - because tape enables us to nest tests for greater readability

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const { createMachine } = require('./index.js')
-const assert = require('assert')
+const test = require('tape')
 
 // BEGIN FIXTURES
 
@@ -45,68 +45,75 @@ const fetchUsersReducer = createMachine({
 
 // END FIXTURES
 
-// BEGIN HELPERS
-
-let state = undefined
-let prevState = undefined
-
-const action = (type, payload) => {
-    prevState = state
-    state = fetchUsersReducer(state, {type, payload})
-}
-
-const expect = (expected, maybeMessage) => assert.deepEqual(state, expected, maybeMessage)
-
-// END HELPERS
-
 // run the reducer and go through several status transitions
 // to test a typical scenario—making an API call
 
-action('DUMMY')
-expect({
-    status: 'INIT',
-}, 'Should set initial status to "INIT"')
+test('should transition between states', t => {
 
-action('FETCH_USERS_RESPONSE', {users})
-expect(prevState, 'Should ignore messages when not handled by current status')
+    let state = undefined
+    let prevState = undefined
 
-action('FETCH_USERS')
-expect({
-    error: null,
-    status: 'IN_PROGRESS'
+    const expect = (expected, maybeMessage) => t.deepEquals(state, expected, maybeMessage)
+
+    const action = (type, payload) => {
+        prevState = state
+        state = fetchUsersReducer(state, {type, payload})
+    }
+
+    action('DUMMY')
+    expect({
+        status: 'INIT',
+    }, 'Should set initial status to "INIT"')
+
+    action('FETCH_USERS_RESPONSE', {users})
+    expect(prevState, 'Should ignore messages when not handled by current status')
+
+    action('FETCH_USERS')
+    expect({
+        error: null,
+        status: 'IN_PROGRESS'
+    })
+
+    action('FETCH_USERS_FAIL', 'timeout')
+    expect({
+        error: 'timeout',
+        status: 'INIT'
+    })
+
+    action('FETCH_USERS')
+    expect({
+        error: null,
+        status: 'IN_PROGRESS'
+    })
+
+    action('FETCH_USERS')
+    expect(prevState)
+
+    action('FETCH_USERS_RESPONSE', {users})
+    expect({
+        error: null,
+        status: 'INIT',
+        users
+    })
+
+    t.end()
 })
 
-action('FETCH_USERS_FAIL', 'timeout')
-expect({
-    error: 'timeout',
-    status: 'INIT'
+test('should error on status not found', t => {
+    let store = {status: 'STATUS_NOT_IN_CREATE_MACHINE'}
+    const reducer = createMachine({})
+    let error
+    try {
+        reducer(store, {type: 'DUMMY'})
+    }
+    catch(err) {
+        error = err
+    }
+
+    t.equals(
+        error.message,
+        'reducersObject missing reducer for status STATUS_NOT_IN_CREATE_MACHINE'
+    )
+
+    t.end()
 })
-
-action('FETCH_USERS')
-expect({
-    error: null,
-    status: 'IN_PROGRESS'
-})
-
-action('FETCH_USERS')
-expect(prevState)
-
-action('FETCH_USERS_RESPONSE', {users})
-expect({
-    error: null,
-    status: 'INIT',
-    users
-})
-
-assert.throws(
-    () => {
-        let store = {status: 'STATUS_NOT_IN_CREATE_MACHINE'}
-        const reducer = createMachine({})
-        store = reducer(store, {type: 'DUMMY'})
-
-    },
-    err => err.message === 'reducersObject missing reducer for status STATUS_NOT_IN_CREATE_MACHINE',
-    'should error when status not found'
-)
-
-console.log('success')
